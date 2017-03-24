1728dd73d1c78cd8a978c94dfe8d9cf8bf923398
Mykhailo Novosad <mykhailo.novosad@itera.no>
2017-03-24 18:13:05 +0200

    Console ML tool

---
+++ ConsoleIncomePrediction/Program.cs
@@ -0,0 +1,86 @@
+﻿// This code requires the Nuget package Microsoft.AspNet.WebApi.Client to be installed.
+// Instructions for doing this in Visual Studio:
+// Tools -> Nuget Package Manager -> Package Manager Console
+// Install-Package Microsoft.AspNet.WebApi.Client
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallRequestResponseService
+{
+
+    public class StringTable
+    {
+        public string[] ColumnNames { get; set; }
+        public string[,] Values { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvokeRequestResponseService().Wait();
+            Console.ReadLine();
+        }
+
+        static async Task InvokeRequestResponseService()
+        {
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+
+                    Inputs = new Dictionary<string, StringTable>() {
+                        {
+                            "input1",
+                            new StringTable()
+                            {
+                                ColumnNames = new string[] {"age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation", "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country"},
+                                Values = new string[,] {  { "40", "A", "20", "higher", "2", "plus", "middle", "married", "w", "m", "40000", "100", "40", "US" },  /*{ "40", "A", "20", "higher", "2", "plus", "middle", "married", "w", "m", "40000", "100", "40", "US" }, */ }
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+                const string apiKey = "QXmP1v54rTqeGhcCe7vi0LAEv1mtk5UqBJa7Pu2jm2UOUYvv+oIh8dwetFF3gJmnzdpMqzuXHvRSukUT8VT9cQ=="; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/ebb331dab9614369af9464e98e947b42/services/172d8cd127804e548f608c03ef838490/execute?api-version=2.0&details=true");
+
+                // WARNING: The 'await' statement below can result1 in a deadlock if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false) so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result1 = await DoSomeTask()
+                // with the following:
+                //      result1 = await DoSomeTask().ConfigureAwait(false)
+
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result1 = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Result: {0}", result1);
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+                    Console.WriteLine(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                }
+            }
+        }
+    }
+}
